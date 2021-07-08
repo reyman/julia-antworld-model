e2d5aa580d3291b608d596b8224e9e4814031378
sebastien.rey-coyrehourcq <sebastien.rey-coyrehourcq@univ-rouen.fr>
2021-07-08 17:48:40 +0200

    New function to run model with or without movie recording

--- ants.jl
+++ ants.jl
@@ -13,9 +13,8 @@ mutable struct Ants <: AbstractAgent
 end
 
 function setup_ants_world(;
-    visual_distance = 5.0,
-    population = 50,
-    speed = 0.5,
+    population = 125,
+    speed = 0.2,
     spacing = visual_distance / 1.5,
     extent = (30, 30),
     seed = 42,
@@ -50,7 +49,7 @@ end
 
 ## STEP ##
 
-function all_model_step!(ants_model, sugar_model)
+function all_model_step!(ants_model, sugar_model, abmstepper)
     Agents.step!(abmstepper, ants_model, ants_agent_step!, ants_model_step!, 1)
     Agents.step!( sugar_model, sugar_agent_step!, sugar_model_step!, 1)
 end
@@ -91,7 +90,7 @@ function ants_agent_step!(ant, model)
             else
                 ant.color = :black
                 # pos = random move
-                if mod(model.tick, 5) == 0
+                if mod(model.tick, 10) == 0
                     #print("modulo tick = $(model.tick) \n")
                     new_pos = get_any_xy(ipos,sugar_model).+ rand(model.rng, Float64)
                     ant.vel = new_pos .- ant.pos
@@ -114,17 +113,17 @@ function ants_agent_step!(ant, model)
             end
 
             print(" Before = Ant $(ant.id) move at step $(model.tick) and speed $(ant.speed) to $(ant.pos)\n")
-            if ant.pos[1] <= ant.speed
-                ant.pos = (1.0,ant.pos[2])
+            if ant.pos[1] <= ant.speed + 1
+                ant.pos = (1.0, ant.pos[2])
             end
-            if ant.pos[2] <= ant.speed
-                ant.pos = (ant.pos[1],1.0)
+            if ant.pos[2] <= ant.speed + 1
+                ant.pos = (ant.pos[1], 1.0)
             end
 
-            if ant.pos[1] >= 29.0
+            if ant.pos[1] >= 29.0 - ant.speed
                 ant.pos = ( 29.0 , ant.pos[2])
             end
-            if ant.pos[2] >= 29.0
+            if ant.pos[2] >= 29.0 - ant.speed
                 ant.pos = (ant.pos[1], 29.0)
             end
             print("After = Ant $(ant.id) move at step $(model.tick) and speed $(ant.speed) to $(ant.pos)\n")
@@ -156,56 +155,97 @@ end
 
 ## MODEL ##
 
-model = setup_ants_world()
-
 using CairoMakie
 using InteractiveDynamics
 
 ## DISPLAY ##
 
 const ants_polygon = Polygon(Point2f0[(-0.2, -0.2), (0.5, 0), (-0.2, 0.2)])
-function ants_marker(b::Ants)
-   φ = atan(b.vel[2], b.vel[1]) #+ π/2 + π
-   scale(rotate2D(ants_polygon, φ), 2)
-end
-
-function ants_color(b::Ants)
-   return b.color
-end
-
-plotkwargs = (
-    am = ants_marker,
-    ac = ants_color,
-)
-fig, abmstepper = abm_plot(model; resolution = (800, 600), plotkwargs...)
-obs_sugar = Observable(model.sugar_model.sugar_landscape)
-obs_chemical = Observable(model.sugar_model.chemical_landscape)
-obs_descent = Observable(model.sugar_model.nest_descent_landscape)
-
-ax, hm = heatmap(fig[1,2], obs_sugar; colormap=cgrad(:thermal))
-ax.aspect = AxisAspect(1)
-
-ax2, hm2 = heatmap(fig[2,1], obs_chemical; colormap=cgrad(:thermal))
-ax2.aspect = AxisAspect(1)
-
-ax3, hm3 = heatmap(fig[2,2], obs_descent; colormap=cgrad(:thermal))
-ax3.aspect = AxisAspect(1)
-
-s = Observable(0) # counter of current step, also observable
-Colorbar(fig[1, 3], hm, width = 20,tellheight=false)
-rowsize!(fig.layout, 1 , ax.scene.px_area[].widths[2])
-fig
 
 # TIPS
 # map tuple julia> map( t -> ((z,(x,y)) = t; (Float16(x), Float16(y))), u)
 
-record(fig, "ants.mp4"; framerate = 20) do io
-    for j in 0:500 # = total number of frames
-        recordframe!(io) # save current state
-        # This updates the abm plot:
-        all_model_step!(model, model.sugar_model)
-        # This updates the heatmap:
-        obs_sugar[] = model.sugar_model.sugar_landscape
-        obs_chemical[] = model.sugar_model.chemical_landscape
+## MAIN
+
+function init_fig(model, observable)
+
+    function ants_marker(b::Ants)
+       φ = atan(b.vel[2], b.vel[1]) #+ π/2 + π
+       scale(rotate2D(ants_polygon, φ), 2)
     end
+
+    function ants_color(b::Ants)
+       return b.color
+    end
+
+    plotkwargs = (
+        am = ants_marker,
+        ac = ants_color,
+    )
+
+    fig, abmstepper = abm_plot(model; resolution = (800, 600), plotkwargs...)
+
+    ax, hm = heatmap(fig[1,2], observable[1]; colormap=cgrad(:thermal))
+    ax.aspect = AxisAspect(1)
+
+    ax2, hm2 = heatmap(fig[2,1], observable[2]; colormap=cgrad(:thermal))
+    ax2.aspect = AxisAspect(1)
+
+    ax3, hm3 = heatmap(fig[2,2], observable[3]; colormap=cgrad(:thermal))
+    ax3.aspect = AxisAspect(1)
+
+    s = Observable(0) # counter of current step, also observable
+    Colorbar(fig[1, 3], hm, width = 20,tellheight=false)
+    rowsize!(fig.layout, 1 , ax.scene.px_area[].widths[2])
+
+    return (fig, abmstepper)
+
 end
+
+function run(model,observable,dorecord)
+
+    # Stop condition
+    rununtil(model, s) = sum(model.sugar_model.sugar_landscape) == 10
+    count_sugar(model) = sum(model.sugar_landscape)
+
+    fig, abmstepper = init_fig(model, observable)
+
+    adata = [:state]
+    mdata = [count_sugar]
+    df_agent = init_agent_dataframe(model, adata)
+    df_model = init_model_dataframe(model.sugar_model, mdata)
+
+    s = 0
+    n = rununtil
+    record(fig, "ants.mp4"; framerate = 20) do io
+
+        while Agents.until(s, rununtil, model)
+            recordframe!(io) # save current state
+            if Agents.should_we_collect(s, model, true)
+                Agents.collect_agent_data!(df_agent, model, adata, s)
+            end
+            if Agents.should_we_collect(s, model.sugar_model, true)
+                Agents.collect_model_data!(df_model, model.sugar_model, mdata, s)
+            end
+            step!(abmstepper, model, ants_agent_step!, ants_model_step!, 1)
+            step!(model.sugar_model, sugar_agent_step!, sugar_model_step!, 1)
+            s += 1
+
+            observable[1][] = model.sugar_model.sugar_landscape
+            observable[2][] = model.sugar_model.chemical_landscape
+        end
+    end
+
+    return df_agent, df_model
+
+    print("mdata = $(mdata)")
+end
+
+model = setup_ants_world()
+
+obs_sugar = Observable(model.sugar_model.sugar_landscape)
+obs_chemical = Observable(model.sugar_model.chemical_landscape)
+obs_descent = Observable(model.sugar_model.nest_descent_landscape)
+observable = [obs_sugar,obs_chemical,obs_descent]
+
+run(model,observable,true)

1014a858e3bfa9e4d736986ffdacde8792eb22f0
sebastien.rey-coyrehourcq <sebastien.rey-coyrehourcq@univ-rouen.fr>
2022-07-07 17:15:02 +0200

    fix for latest agents.jl, julia 1.7

--- main.jl
+++ main.jl
@@ -3,8 +3,12 @@
 include("ants.jl")
 include("sugar.jl")
 
+using Agents
 using CairoMakie
 using InteractiveDynamics
+using Makie.GeometryBasics
+
+CairoMakie.activate!()
 
 ## Const
 const ants_polygon = Polygon(Point2f0[(-0.2, -0.2), (0.5, 0), (-0.2, 0.2)])
@@ -15,35 +19,31 @@ const ants_polygon = Polygon(Point2f0[(-0.2, -0.2), (0.5, 0), (-0.2, 0.2)])
 function init_fig(model, observable)
 
     function ants_marker(b::Ants)
-       φ = atan(b.vel[2], b.vel[1]) #+ π/2 + π
-       scale(rotate2D(ants_polygon, φ), 2)
+        ^f = atan(b.vel[2], b.vel[1]) #+  ^`/2 +  ^`
+       scale(rotate2D(ants_polygon,  ^f), 2)
     end
 
     function ants_color(b::Ants)
        return b.color
     end
 
-    plotkwargs = (
-        am = ants_marker,
-        ac = ants_color,
-    )
+    fig, abmstepper, abmobs = abmplot(model; agent_step! = ants_agent_step!,model_step! = ants_model_step!, am= ants_marker, ac=ants_color)
 
-    fig, abmstepper = abm_plot(model; resolution = (800, 600), plotkwargs...)
 
     ax, hm = heatmap(fig[1,2], observable[1]; colormap=cgrad(:thermal))
     ax.aspect = AxisAspect(1)
 
-    ax2, hm2 = heatmap(fig[2,1], observable[2]; colormap=cgrad(:thermal))
+    ax2, hm2 = heatmap(fig[2,2][1,1], observable[2]; colormap=cgrad(:thermal))
     ax2.aspect = AxisAspect(1)
 
-    ax3, hm3 = heatmap(fig[2,2], observable[3]; colormap=cgrad(:thermal))
+    ax3, hm3 = heatmap(fig[2,2][1,2], observable[3]; colormap=cgrad(:thermal))
     ax3.aspect = AxisAspect(1)
 
     s = Observable(0) # counter of current step, also observable
     Colorbar(fig[1, 3], hm, width = 20,tellheight=false)
     rowsize!(fig.layout, 1 , ax.scene.px_area[].widths[2])
 
-    return (fig, abmstepper)
+    return (fig, abmstepper, abmobs)
 
 end
 
@@ -53,8 +53,7 @@ function myrun(model,observable,stopWhenSugarEqual, dorecord)
     rununtil(model, s) = sum(model.sugar_model.sugar_landscape) == stopWhenSugarEqual
     count_sugar(model) = sum(model.sugar_landscape)
 
-    fig, abmstepper = init_fig(model, observable)
-
+    fig, abmstepper, abmobs = init_fig(model, observable)
     adata = [:state]
     mdata = [count_sugar]
     df_agent = init_agent_dataframe(model, adata)
@@ -67,14 +66,16 @@ function myrun(model,observable,stopWhenSugarEqual, dorecord)
         while Agents.until(s, rununtil, model)
             print("sugar remaining on world =  $(sum(model.sugar_model.sugar_landscape)) \n")
             recordframe!(io) # save current state
+
             if Agents.should_we_collect(s, model, true)
                 Agents.collect_agent_data!(df_agent, model, adata, s)
             end
             if Agents.should_we_collect(s, model.sugar_model, true)
                 Agents.collect_model_data!(df_model, model.sugar_model, mdata, s)
             end
-            step!(abmstepper, model, ants_agent_step!, ants_model_step!, 1)
+            step!(abmobs,1)
             step!(model.sugar_model, sugar_agent_step!, sugar_model_step!, 1)
+
             s += 1
 
             observable[1][] = model.sugar_model.sugar_landscape
@@ -106,4 +107,4 @@ function main_ants(population, evaporationRate, diffusionRate, seed, stopWhenSug
 end
 
 # Testing
-#main_ants(250, 2, 30, 42, 30 )
+main_ants(ARGS[1], ARGS[2], ARGS[3], ARGS[4], ARGS[5] )
